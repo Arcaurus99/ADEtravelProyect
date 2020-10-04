1ac8052e5080bdc85166d89a609cb9828e7bb1d7
Esteban <estabanar@unicauca.edu.co>
2020-10-03 21:54:59 -0500

    Este es el primer test del proyecto oficial

---
+++ src/main/java/control/Agencia.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Agencia {
+    
+    public String buscarPaquete(){
+        
+        //----CONEXIÓN SEVIDOR----
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017"); 
+        //----CONEXIÓN A BD----
+        MongoDatabase database = mongoClient.getDatabase("ADEtravel");
+        //----GENERAR COLECCIÓN----
+        MongoCollection<Document> collection = database.getCollection("paquetes");
+        
+        //AGREGAR EXCEPCIONES DE ELEMENTO VACÍO
+        
+        return collection.find().first().toJson();
+    }
+    
+}
