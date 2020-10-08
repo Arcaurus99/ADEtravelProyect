8414c21857a311771e6bb45184fb023667869dfc
Andres Fernandez <andresfernandez@unicauca.edu.co>
2020-10-08 18:13:39 -0500

    Conexión remota exitosa

--- src/main/java/control/Agencia.java
+++ src/main/java/control/Agencia.java
@@ -10,26 +10,27 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-//ARNULFITO GAY ..
+
 /**
  *
  * @author Esteban
  */
 public class Agencia {
     
+
+    //----CONEXICÓN REMOTA Y LOCAL----
+    MongoClient mongoClient = MongoClients.create("mongodb://userLab4:passworduserLab4@93.188.167.110:27017/?authSource=lab4");
+    //MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+    //----CONEXIÓN A BD----
+    MongoDatabase database = mongoClient.getDatabase("lab4");
+    
     public String buscarPaquete(){
-        
-        //----CONEXIÓN SEVIDOR----
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017"); 
-        //----CONEXIÓN A BD----
-        MongoDatabase database = mongoClient.getDatabase("ADEtravel");
+      
         //----GENERAR COLECCIÓN----
         MongoCollection<Document> collection = database.getCollection("paquetes");
-        //hola mundo
         
         //AGREGAR EXCEPCIONES DE ELEMENTO VACÍO
-        
         return collection.find().first().toJson();
     }
-    
+        
 }
