a6a9170491d4e75a43aa68197a2e1955a5cd8633
Esteban <estabanar@unicauca.edu.co>
2020-10-09 05:38:10 -0500

    update document test method part 2

--- src/main/java/control/Agencia.java
+++ src/main/java/control/Agencia.java
@@ -9,27 +9,47 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.result.*;
 import org.bson.Document;
-//ARNULFITO GAY ..
+import org.bson.types.ObjectId;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Updates.*;
+
+
 /**
  *
  * @author Esteban
  */
 public class Agencia {
     
+
+    //----CONEXICÓN REMOTA Y LOCAL----
+    MongoClient mongoClient = MongoClients.create("mongodb://userLab4:passworduserLab4@93.188.167.110:27017/?authSource=lab4");
+    //MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+    //----CONEXIÓN A BD----
+    MongoDatabase database = mongoClient.getDatabase("lab4");
+    
     public String buscarPaquete(){
-        
-        //----CONEXIÓN SEVIDOR----
-        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017"); 
-        //----CONEXIÓN A BD----
-        MongoDatabase database = mongoClient.getDatabase("ADEtravel");
+      
         //----GENERAR COLECCIÓN----
         MongoCollection<Document> collection = database.getCollection("paquetes");
-        //hola mundo
         
         //AGREGAR EXCEPCIONES DE ELEMENTO VACÍO
-        
         return collection.find().first().toJson();
     }
     
+    public String actualizarCliente(String id, String address, String movil){
+        
+        MongoCollection<Document> collection = database.getCollection("cliente");
+        collection.updateOne(eq("_id", new ObjectId(id)), combine(set("address", address), set("movil", movil)));
+        return "(\"Confirmation\": 1)";
+        
+    }
+    
+        
 }
